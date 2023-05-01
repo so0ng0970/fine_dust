c1feca83f37cc69184d759fd7c80926ea5064b92
soonger3306 <soonger3306@daum.net>
2023-05-01 21:48:06 +0900

    SliverAppBar,

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,11 +1,20 @@
-import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:fine_dust/component/main_app_bar.dart';
+import 'package:fine_dust/const/colors.dart';
+import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Scaffold(
+      backgroundColor: primaryColor,
+      body: CustomScrollView(
+        slivers: [
+          MainAppBar(),
+        ],
+      ),
+    );
   }
 }
