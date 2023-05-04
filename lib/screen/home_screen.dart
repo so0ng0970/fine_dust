b7afd2167e9ef32a4293b8e824967dde4d165bc7
soonger3306 <soonger3306@daum.net>
2023-05-05 00:04:22 +0900

    MainCard

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -29,9 +29,50 @@ class HomeScreen extends StatelessWidget {
                 MainCard(
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: const [
-                      CardTitle(
+                    children: [
+                      const CardTitle(
                         title: '시간별 미세먼지',
+                      ),
+                      Column(
+                        children: List.generate(
+                          24,
+                          (index) {
+                            final now = DateTime.now();
+                            final hour = now.hour;
+                            int currentHour = hour - index;
+                            // 3 2 1 0 23 22 21
+                            if (currentHour < 0) {
+                              currentHour += 24;
+                            }
+                            return Padding(
+                              padding: const EdgeInsets.symmetric(
+                                horizontal: 8.0,
+                                vertical: 4.0,
+                              ),
+                              child: Row(
+                                children: [
+                                  Expanded(
+                                    child: Text(
+                                      currentHour.toString(),
+                                    ),
+                                  ),
+                                  Expanded(
+                                    child: Image.asset(
+                                      'asset/image/good.png',
+                                      height: 20.0,
+                                    ),
+                                  ),
+                                  const Expanded(
+                                    child: Text(
+                                      '좋음',
+                                      textAlign: TextAlign.end,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            );
+                          },
+                        ),
                       )
                     ],
                   ),
