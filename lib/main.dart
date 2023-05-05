4b243424f4e29522d135d252336b2bfe18b7d3c1
soonger3306 <soonger3306@daum.net>
2023-05-05 22:22:39 +0900

    main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(fontFamily: 'sunflower'),
+      theme: ThemeData(fontFamily: 'sunflower '),
       home: const HomeScreen(),
     );
   }
