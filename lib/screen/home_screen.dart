abe71c7245cbae2f97f0c2bfc20f086838fa685a
soonger3306 <soonger3306@daum.net>
2023-05-08 18:44:30 +0900

    dio().get

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,14 +1,42 @@
+import 'package:dio/dio.dart';
 import 'package:fine_dust/component/category_card.dart';
 import 'package:fine_dust/component/hourly_card.dart';
 import 'package:fine_dust/component/main_app_bar.dart';
 import 'package:fine_dust/component/main_drawer.dart';
 import 'package:fine_dust/const/colors.dart';
+import 'package:fine_dust/const/data.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async {
+    final response = await Dio().get(
+      dataUrl,
+      queryParameters: {
+        'serviceKey': serviceKey,
+        'returnType': 'json',
+        'numOfRows': 30,
+        'pageNo': 1,
+        'itemCode': 'PM10',
+        'dataGubun': 'HOUR',
+        'searchCondition': 'WEEK'
+      },
+    );
+    print(response.data);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
