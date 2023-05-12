c11cdecc466355e940e45523c5a0a8569129abf4
soonger3306 <soonger3306@daum.net>
2023-05-12 17:36:38 +0900

    hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:fine_dust/screen/test_screen.dart';
+import 'package:fine_dust/model/stat_model.dart';
+import 'package:fine_dust/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
@@ -6,7 +7,14 @@ const testBox = 'test';
 void main() async {
   await Hive.initFlutter();
 
+  Hive.registerAdapter<StatModel>(StatModelAdapter());
+  Hive.registerAdapter<ItemCode>(ItemCodeAdapter());
+
   await Hive.openBox(testBox);
+
+  for (ItemCode itemCode in ItemCode.values) {
+    await Hive.openBox(itemCode.name);
+  }
 
   runApp(const MyApp());
 }
@@ -18,7 +26,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(fontFamily: 'sunflower'),
-      home: const TestScreen(),
+      home: const HomeScreen(),
     );
   }
 }
