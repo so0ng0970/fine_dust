8510ced01cd8eccd9972214c726ac5be8bc35579
soonger3306 <soonger3306@daum.net>
2023-05-14 22:16:08 +0900

    category_card

--- lib/component/category_card.dart
+++ lib/component/category_card.dart
@@ -1,8 +1,10 @@
 import 'package:fine_dust/component/card_title.dart';
 import 'package:fine_dust/component/main_card.dart';
 import 'package:fine_dust/component/main_stat.dart';
+import 'package:fine_dust/model/stat_model.dart';
 import 'package:fine_dust/utils/data_utils.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 
 class CategoryCard extends StatelessWidget {
   final String region;
@@ -36,19 +38,32 @@ class CategoryCard extends StatelessWidget {
                     scrollDirection: Axis.horizontal,
                     // 조금만 넘겨도 다음페이지 보여줌
                     physics: const PageScrollPhysics(),
-                    children: models
+                    children: ItemCode.values
                         .map(
-                          (model) => MainStat(
-                            width: constraint.maxWidth / 3,
-                            category: DataUtils.getItemCodeKrString(
-                                itemcode: model.itemCode),
-                            imgPath: model.status.imagePath,
-                            level: model.status.label,
-                            stat: '${model.stat.getLevelFromRegion(
-                              region,
-                            )} ${DataUtils.getUnitFromItemCode(
-                              itemcode: model.itemCode,
-                            )}',
+                          (ItemCode itemCode) => ValueListenableBuilder<Box>(
+                            valueListenable:
+                                Hive.box<StatModel>(itemCode.name).listenable(),
+                            builder: (context, box, widget) {
+                              final stat = (box.values.last as StatModel);
+                              final status =
+                                  DataUtils.getStatusFromItemCodeAndValue(
+                                itemCode: itemCode,
+                                value: stat.getLevelFromRegion(region),
+                              );
+
+                              return MainStat(
+                                width: constraint.maxWidth / 3,
+                                category: DataUtils.getItemCodeKrString(
+                                    itemcode: itemCode),
+                                imgPath: status.imagePath,
+                                level: status.label,
+                                stat: '${stat.getLevelFromRegion(
+                                  region,
+                                )} ${DataUtils.getUnitFromItemCode(
+                                  itemcode: itemCode,
+                                )}',
+                              );
+                            },
                           ),
                         )
                         .toList(),
